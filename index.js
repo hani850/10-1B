b673166613ff8e4df5e14508fffb0e7e3e5c9149
hani850 <52225021+hani850@users.noreply.github.com>
2025-05-21 23:52:39 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const path = require('path');
 
 let sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('myDB', (err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.message);
-  } else {
-    console.log('Connected to the SQLite database.');
-  }
+    if (err) {
+        console.error('Error connecting to the database:', err.message);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
 });
 
 db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS User(id INTEGER PRIMARY KEY AUTOINCREMENT, fname TEXT, sname TEXT, email TEXT, date TEXT NOT NULL, q1 INTEGER, q2 INTEGER, q3 INTEGER, colour TEXT, comment TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS User(id INTEGER PRIMARY KEY AUTOINCREMENT, fname TEXT, sname TEXT, email TEXT, date TEXT, q1 INTEGER, q2 INTEGER, q3 INTEGER, colour TEXT, comment TEXT)");
     db.run("DELETE FROM User");
 
     console.log('New database cleared');
 
-     db.run(`INSERT INTO User (id, fname, sname, email, date, q1, q2, q3, colour, comment) 
+    db.run(`INSERT INTO User (id, fname, sname, email, date, q1, q2, q3, colour, comment) 
         VALUES ("1", "su","leong", "email1", "1/24", "2", "2","5","blue","A butterfly landed on me")`,)
-         console.log('New database values inserted');
+    console.log('New database values inserted');
 });
 
 const app = express();
@@ -112,7 +112,8 @@ app.post('/submitsurvey', (req, res, next) => {
     currentTime = currentTime.toString();
     submissionCount++;
     let now = new Date();
-    console.log( now.toISOString() );
+    var isoString = now.toISOString();
+    console.log("outputting date: " + isoString);
 
     /* //EXAMPLE DB INSERT
     cur.execute(
@@ -122,19 +123,17 @@ app.post('/submitsurvey', (req, res, next) => {
     //Add items into the DB
     db.serialize(function () {
         db.run(`INSERT INTO User (fname, sname, email, date, q1, q2, q3, colour, comment) VALUES (?,?,?,?,?,?,?,?,?)`,
-            (userFirstname, userLastname, userEmail, now, userQ1, userQ2, userQ3, userFavColour, userComments),)
-             console.log('Survey complete: Database Updated');
+            (userFirstname, userLastname, userEmail, isoString, userQ1, userQ2, userQ3, userFavColour, userComments),)
+        console.log('Survey complete: Database Updated');
 
-
-        db.each('SELECT * FROM Comments', function (err, row) {
+        db.each('SELECT * FROM User', function (err, row) {
             if (err) {
-                console.log(err.message);
+                console.log("Can't Select from USER" + err.message);
                 throw err;
             }
             console.log(`[all] Name: ${row.fname}, Surname: ${row.sname}`);
         });
     });
-    db.close();
     //db / displayDB.js
 
 
@@ -154,51 +153,47 @@ app.post('/submitsurvey', (req, res, next) => {
             Q3: userQ3,
         });
     //send back a response
-    res.send('Thank you for submitting the form data');
+    // res.send('Thank you for submitting the form data');
 });
 
-app.get('/users', function (req, res) {
+app.get('/user', function (req, res) {
     let html = '';
+
     //HTML code to display multiple tables with DB data
-        html += '<body><div class="border border-primary rounded mb-3 p-2">';
+    html += '<header><div class="container-fluid bg-success-subtle">';
+    html += '<div class="col-sm-8 mx-auto text-center py-4">';
+    html += '<h1 class="display-6">dKin Butterfly Club</h1>';
+    html += '<p class="lead">Informative web page on Butterflies from around the world</p> </div></div></header>';
 
-        // Retrieve data from table User on the server 
-        // and display it in a web page table structure
-        db.all('SELECT * FROM User', function (err, rows) {
-            if (err) {
-                return console.error(err.message);
-            }
-            if (rows.length === 0) {
-                console.log("Array is empty!")
+    html += '<main> <div class="container-fluid p-3 py-4"><h1>Survey List</h1>';
+    html += '<p>The complete list of <em>dKin Butterflies</em> surveys is shown in the table below. Accessing this data stored in a persistent database is performed on the server. <br> ';
+    html += 'There is a total of ' + submissionCount + ' surveys completed.</p>';
 
-                html += '<header><div class="container-fluid bg-success-subtle">';
-                html += ' <div class="col-sm-8 mx-auto text-center py-4">';
-                html += '<h1 class="display-6">dKin Butterfly Club</h1>';
-                html += ' <p class="lead">Informative web page on Butterflies from around the world</p> </div></div></header>';
-               
-                html += '<main> <div class="container-fluid p-3 py-4"><h1>Survey List</h1>';
-                html += '<p>The complete list of <em>dKin Butterflies</em> surveys is shown in the table below. Accessing this data stored in a persistent database is performed on the server. <br> ';
-                html += 'There is a total of <%= surveysCompleted%> surveys completed.</p>';
+    // Retrieve data from table User on the server 
+    // and display it in a web page table structure
+    db.all('SELECT * FROM User', function (err, rows) {
+        if (err) {
+            return console.log(err.message);
+        }
+        if (rows.length === 0) {
+            console.log("Array is empty!")
+        } else {
+            rows.forEach(function (row) {
+                html += '<div class="border border-primary rounded mb-3 p-2"><p class="text-secondary mb-1">';
+                html += '<small>' + row.id + '  ' + row.fname + '  ' + row.sname + '</small></p>';
+                html += '<div class="row"><div class="col-sm-6">';
+                html += '<p class="text-primary mb-0">Comment</p><p><em>' + row.comment + '</em></p></div>';
+                html += '<div class="col-sm-2"><p class="text-primary mb-0">Colour</p><div class="badge text-wrap" style="background-color:' + row.colour + '">' + row.colour + '</div></div>';
+                html += '<div class="col-sm-4"><p class="text-primary mb-0">Results</p><div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="' + row.q1 + `"`;
+                html += 'aria-valuemin="1" aria-valuemax="5">Q1<div class="progress-bar text-bg-success" style="width:60%">' + row.q1 + '</div></div>';
+                html += '<div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="' + row.q2 + '" aria-valuemin="1" aria-valuemax="5">Q2<div class="progress-bar text-bg-success" style="width:20%">' + row.q2 + '</div></div>';
+                html += '<div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="' + row.q3 + '" aria-valuemin="1" aria-valuemax="5"> Q3<div class="progress-bar text-bg-success" style="width:' + (row.q3 / 5) * 100 + '%">' + row.q3 + '</div></div>';
+                html += '</div></div><p class="text-secondary mb-1"><small>' + row.date + '</small></p></div>';
+            });
+        }
+        html += '</div></main>';
 
-                html += '<body>';
-            } else {
-                rows.forEach(function (row) {
-                    html += '<div class="border border-primary rounded mb-3 p-2"><p class="text-secondary mb-1">';
-                    html += '<small>' + row.id + row.userFirstname + row.userLastname + '</small></p>';
-                    html += '<div class="row"><div class="col-sm-6">';
-                    html += '<td>' + '</td>';
-                    html += '<p class="text-primary mb-0">Comment</p><p><em>' + row.userComments + '</em></p></div>';
-                    html += '<div class="col-sm-2"><p class="text-primary mb-0">Colour</p><div class="badge text-wrap" style="background-color:' + row.userFavColour + '">' + row.userFavColour +'</div></div>';
-                    html += '<div class="col-sm-4"><p class="text-primary mb-0">Results</p><div class="progress" role="progressbar" aria-label="Success example"aria-valuenow="' + row.userQ1;
-                    html += '" aria-valuemin="1" aria-valuemax="5">Q1<div class="progress-bar text-bg-success" style="width:60%">' + row.userQ1 + '</div></div>';
-                    html += '<div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="' + row.userQ2 + '" aria-valuemin="1" aria-valuemax="5">Q2<div class="progress-bar text-bg-success" style="width:20%">' + row.userQ2 + '</div></div>';
-                    html += '<div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="' + row.userQ3 + '" aria-valuemin="1" aria-valuemax="5"> Q3<div class="progress-bar text-bg-success" style="width:100%">' + row.userQ2 + '</div></div>';
-                    html += '</div></div><p class="text-secondary mb-1"><small>' + row.currentTime + '</small></p></div>';
-                });
-            }
-            html += '</body></div></main>';
-            
-         console.log('New database Outputed in Surveys');
+        console.log('New database Outputed in Surveys');
         res.send(html);
 
     });
